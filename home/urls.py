f93a9e6ec5eb1eeb47e1187e7a77c396826e1e22
mmadi <tajik00sh@gmail.com>
2025-12-08 11:25:09 +0330

    anjamshod alii

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from home.views import home
+from home.views import home, home2
 
 
 
 urlpatterns = [
     path('', home, name='home'),
+    path('home2/',home2, name='home2' )
 
 
 ]
--- home/views.py
+++ home/views.py
@@ -3,3 +3,8 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "home/home.html")
+
+
+
+def home2(request):
+    return render(request, "home/home2.html")
